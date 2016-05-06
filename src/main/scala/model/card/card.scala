1cfa26b725272a26f1967e321973962ce96c8497
Noah Ogrydziak <Nrogrydziak@gmail.com>
2016-05-06 09:41:34 -0400

    inline comments to card class

--- src/main/scala/model/card/card.scala
+++ src/main/scala/model/card/card.scala
@@ -1,15 +1,19 @@
 package model.card
 
-/** a playing card in the game
-  *
-  * @constructor create a new card with id, face, and suit
-  * @param id the integer id of the card (2-14)
-  * @param face the string representation of the id
-  * @parm suit the string representation of the card suit
-  */
+/* class for a playing card
+id: Integer from 2 to 14
+
+face: string representation of id
+  2-10 map to integer representation
+    ex) 2 -> "2", 3 -> "3", 4 -> "4", ..., 10 -> "10"
+  11-14 map to playing card face
+    ex) 11 -> "J", 12 -> "Q", 13 -> "K", 14 -> "A"
+
+suit: string representation of card suit
+  There are 4
+  Heart -> "H", Diamond -> "D", Spade -> "S", Club -> "C"
+*/
 case class Card(val id: Int, val face: String, val suit: String) {
-
-  /** display a card. ex) 2 of Hearts => [2-H] */
+  //string representation: ex) 2 of Hears -> "[2-H]"
   override def toString: String = s"[$face-$suit]"
-
 }
--- src/main/scala/model/deck/deck.scala
+++ src/main/scala/model/deck/deck.scala
@@ -3,6 +3,13 @@ package model.deck
 import model.card._
 import scala.collection.mutable.Queue
 
+/** a playing card in the game
+  *
+  * @constructor create a new card with id, face, and suit
+  * @param id the integer id of the card (2-14)
+  * @param face the string representation of the id
+  * @parm suit the string representation of the card suit
+  */
 class deck extends Queue[Card] {
 
   override def toString: String = this.mkString(", ")
