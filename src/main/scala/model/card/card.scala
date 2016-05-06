44d7dc86b175748d914cfc26fc7691ebef33efcf
Noah Ogrydziak <Nrogrydziak@gmail.com>
2016-05-06 09:19:20 -0400

    add documentation to card class package

--- src/main/scala/model/card/card.scala
+++ src/main/scala/model/card/card.scala
@@ -1,8 +1,15 @@
 package model.card
 
-//####Card Class####
+/** a playing card in the game
+  *
+  * @constructor create a new card with id, face, and suit
+  * @param id the integer id of the card (2-14)
+  * @param face the string representation of the id
+  * @parm suit the string representation of the card suit
+  */
 case class Card(val id: Int, val face: String, val suit: String) {
 
-  override def toString: String = "[" + face + "-" + suit + "]"
+  /** display a card. ex) 2 of Hearts => [2-H] */
+  override def toString: String = s"[$face-$suit]"
 
 }
