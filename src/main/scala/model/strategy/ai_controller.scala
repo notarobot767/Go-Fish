7ab6ae51354fb6f446ec1207b3a891f6c7a07eab
Noah Ogrydziak <Nrogrydziak@gmail.com>
2016-05-14 20:30:23 -0400

    move adding strat id mapping outside of initialize

--- src/main/scala/model/strategy/ai_controller.scala
+++ src/main/scala/model/strategy/ai_controller.scala
@@ -8,6 +8,7 @@ package model.strategy
 
 import model.data._
 import model.loader._
+import model.statics.Statics
 
 import scala.collection.mutable.Map
 
@@ -20,17 +21,12 @@ class AI_Controller(data: Stencil, loader: Loader) {
 
   //map id to strat
   private val strat_idMap = Map[Int, Strat]()
+  private var i = 1
+  AI_Functions(data, loader).foreach({s => strat_idMap += i -> s; i += 1})
   def get_strat_idMap: Map[Int, Strat] = this.strat_idMap
 
   //map each player to default strat ID
   def initialize = {
-    var i = 1
-    strat_idMap.clear
-    AI_Functions(data, loader).foreach({s =>
-      strat_idMap += i -> s
-      i += 1
-    })
-
     strat_playerMap.clear
     players.foreach(p =>
       strat_playerMap += p -> strat_idMap(Statics.default_strat))
