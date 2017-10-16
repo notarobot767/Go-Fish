523cfd638facd95a3dddaa1cd9ff458d0c0e6dd9
notarobot767 <notarobot767@users.noreply.github.com>
2017-10-15 22:25:37 -0400

    basic player draw

--- src/main/scala/controller/playerctl.scala
+++ src/main/scala/controller/playerctl.scala
@@ -2,6 +2,7 @@ package controller
 
 import model._
 import model.players._
+import model.playing_cards._
 import scala.collection.mutable.Map
 
 class Playerctl (players: Players, deckctl: Deckctl) {
@@ -18,14 +19,25 @@ class Playerctl (players: Players, deckctl: Deckctl) {
   //who is up next
   def upNext: String = players.tail.head.toString
   
-  //who is in the lead
-  def leader: String = {
-    //get leaderbard then parse
-    ???
+  //list of highest highscores
+  private getLeader: List[String] = {
+    val leaderboard = Map[Int -> List[String]]
+    players.foreach(p => lead += p.points +: leaderboard.getOrElse(p.points, List[String]()))
+    leaderboard.max._2
   }
-  
+ 
   //draw from deck and put card in hand of current player
-  def draw = ???
+  //false means no cards to draw
+  def draw: Boolean = {
+    if(deckctl.isEmpty) false
+    else {
+      val card = deckctl.draw
+      val hand = players.head.hand
+      hand += (card.id -> card +: hand.getOrElse(card.id, List[Card]))
+      //check for 4
+      true
+    }
+  }
   
   def show: String = {
     var ans_str = ""
